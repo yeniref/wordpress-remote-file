a0bb263e5ba10fcd7445d556b39966c1af5e964c
Murat <85807376+yeniref@users.noreply.github.com>
2023-05-07 14:21:25 +0300

    Update DosyaYukle.php

--- DosyaYukle.php
+++ DosyaYukle.php
@@ -4,46 +4,48 @@ include 'wp-config.php';
 
 class FileUploadRemote {
 
-   public function DosyaYukle($file, $baslik = NULL)
+    public function DosyaYukle($file_url, $baslik = NULL)
     {
-
         $baslik = ($baslik == NULL) ? md5(time()) : $baslik;
-        $filetype = wp_check_filetype(basename($file), null);
+        $filename = basename($file_url);
+        $filetype = wp_check_filetype($filename, null);
         $uzanti = $filetype['ext'];
         $filename = sanitize_title($baslik) . md5(rand(0, 50)) . "." . $uzanti;
-
-        $upload = wp_upload_bits($filename, 'null', file_get_content($file));
-        if (empty($upload['error'])) {
+    
+        $upload_dir = wp_upload_dir();
+        $target_file = $upload_dir['path'] . '/' . basename($filename);
+    
+        // Dosyayı yükleme işlemi
+        $file_contents = file_get_contents($file_url);
+        if (file_put_contents($target_file, $file_contents)) {
             $attachment = array(
-                'guid'           => $upload['url'],
+                'guid'           => $upload_dir['url'] . '/' . basename($target_file),
                 'post_mime_type' => $filetype['type'],
-                'post_title'     => preg_replace('/\.[^.]+$/', '', basename($filename)),
+                'post_title'     => preg_replace('/\.[^.]+$/', '', basename($baslik)),
                 'post_content'   => '',
                 'post_status'    => 'inherit'
             );
-
-            $attach_id = wp_insert_attachment($attachment, $upload['file'], 0);
+            $attach_id = wp_insert_attachment($attachment, $target_file, 0);
             require_once(ABSPATH . 'wp-admin/includes/image.php');
             require_once(ABSPATH . 'wp-admin/includes/media.php');
             require_once(ABSPATH . 'wp-admin/includes/file.php');
-            $attach_data = wp_generate_attachment_metadata($attach_id, $upload['file']);
-            foreach ($attach_data['sizes'] as $boyutlar) {
-                $boyut[] = $boyutlar['file'];
-            }
+            $attach_data = wp_generate_attachment_metadata($attach_id, $target_file);
             wp_update_attachment_metadata($attach_id, $attach_data);
-            return array(
+    
+            $data = array(
                 'id' => $attach_id,
-                'url' => $upload['url'],
-                'file' => $upload['file'],
-                'boyutlar' => $boyut,
-                'baslik' => $baslik . substr(md5(time()), 0, 4),
-                'rbaslik' => $attach_data['file'],
-                'uzanti' => $uzanti
+                'url' => $upload_dir['url'] . '/' . basename($target_file),
+                'file' => $target_file,
+                'boyutlar' => $attach_data['sizes'],
+                'baslik' => $baslik,
+                'filename' => $attach_data['file'],
+                'uzanti' => pathinfo($target_file, PATHINFO_EXTENSION)
             );
+            return $data;
         } else {
-            return "false"; //File Does Not Exist, Access Denied, URL Moved, etc.
+            return "Hata: Dosya yüklenirken bir sorun oluştu.";
         }
-    }
+    }    
 }
 
 $yukle = new FileUploadRemote;
